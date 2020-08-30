d311728ea6ffb073d5dcf0746c9ead37f0a8ce35
Ezenwankwo Ugochukwu <ugoezenwankwo@gmail.com>
2020-08-30 07:00:52 +0100

    Finalize major logic for file hnadling of jpeg, jpg, png, gif, mp3, mp4, and pdf

--- djfh.js
+++ djfh.js
@@ -8,7 +8,7 @@ let djfhDropFile = false;
 
 djfhForm.addEventListener('submit', function(e){
     e.preventDefault();
-    upload(); //  use function if input is not empty
+    preview(); //  use function if input is not empty
 })
 
 let isDjfhInitialized = false;
@@ -39,9 +39,9 @@ const isFileSelected = () => {
 }
 
 /**
- * Preview File prior to upload
+ * Preview File
  */
-const upload = () => {
+const preview = () => {
     if(!isDjfhInitialized){
         // var error = new Object();
         // error.name = `Please select a file`;
@@ -60,36 +60,57 @@ const upload = () => {
     }else{
         file = djfhDropFile;
     }
+    // console.log(file)
     
-    let size = file.size;
-    let name = file.name;
-    let type = file.type;
-    console.log(type);
+    let blobURL = URL.createObjectURL(file);
+    // console.log(blobURL)
+    let size = file.size; // File size
+    let name = file.name; // File name
+    let type = file.type; // File type
 
-    let result = validateType(name, size);
+    let allDots = name.split("."); // Split filename to extract actual file extension
+
+    let fileExtension = allDots[allDots.length - 1]; // Get the actual file extension
+
+    let result = validateType(fileExtension, size);
     
     if(!result){
         return addErrors();
     }
     if(result){
         let base64_img = 'prev';
-        if(createPreviewElement('embed', 'success', type)){
-            // if(createPreviewElement('img', 'success')){
+        let tagToCreate = 'p';
+
+        //  Set the HTML tag to create based on the file extension
+        if(fileExtension == 'pdf' || fileExtension == 'csv' || fileExtension == 'xlsx'){
+            tagToCreate = 'embed';
+        }else if(fileExtension == 'png' || fileExtension == 'jpg' || fileExtension == 'jpeg' || fileExtension == 'gif' || fileExtension == 'webp'){
+            tagToCreate = 'img';
+        }else if(fileExtension == 'mp3'){
+            tagToCreate = 'audio';
+        }else if(fileExtension == 'mp4'){
+            tagToCreate = 'video';
+        }
+
+        if(createPreviewElement(tagToCreate, 'success', type)){
+            let blobURL = URL.createObjectURL(file);
+            let prev = document.getElementById('preview-element');
+            prev.src = blobURL;
+            return djfhDropbox.style.background = "#ccffdd";
             
-            var reader = new FileReader();
-            reader.onloadend = function() {
-                base64_img = reader.result;
-                console.log(base64_img);
-                let prev = document.getElementById('preview-element');
-                prev.src = base64_img;
+            // var reader = new FileReader();
+            // reader.onloadend = function() {
+            //     base64_img = reader.result;
+            //     // console.log(base64_img);
+            //     let prev = document.getElementById('preview-element');
+            //     prev.src = base64_img;
                 
                 
-            }
-            reader.readAsDataURL(file);
-            djfhDropbox.style.background = "#ccffdd";
-            // document.getElementsByTagName("FORM")[0].style.display = "block";
+            // }
+            // reader.readAsDataURL(file);
+            // djfhDropbox.style.background = "#ccffdd";
             
-            uploadToServer(file);
+            // uploadToServer(file);
         }
     }
 }
@@ -100,11 +121,7 @@ const upload = () => {
  * @param {Name of file} name 
  * @param {Size of file} size 
  */
-const validateType = (name, size) => {
-    let allDots = name.split(".");
-
-    let fileType = allDots[allDots.length - 1];
-    console.log(fileType)
+const validateType = (fileType, size) => {
     
     if(types.indexOf(fileType) === -1 && types.length > 0){
         // var error = new Object();
@@ -172,20 +189,28 @@ const displayError = (message = null) => {
  */
 const createPreviewElement = (tag, type, fileType = '') => {
     // djfhPreview.addEventListener
-    
-    let previewElement = document.createElement(tag);
+    let previewElement = '';
+    djfhPreview.innerHTML = ''; // Cleat all fields
+    if(tag == 'audio'){
+        previewElement = '<audio controls><source id="preview-element" type="audio/mpeg">Your browser does not support the audio element.</audio>';
+        return djfhPreview.innerHTML = previewElement;
+    }
+    if(tag == 'video'){
+        previewElement = '<video controls><source id="preview-element" type="audio/mpeg">Your browser does not support the audio element.</video>';
+        return djfhPreview.innerHTML = previewElement;
+    }
+    previewElement = document.createElement(tag);
+    // console.log(previewElement)
     previewElement.setAttribute('id', type == 'success' ? 'preview-element' : 'error-element');
     previewElement.setAttribute('class', 'animate__animated animate__zoomIn');
     //  If a file type is specified, add the file type to the embed attribute
     if(fileType != ''){
         previewElement.setAttribute('type', fileType);
     }
-    // console.log(djfhPreview);
-    // return console.log(previewElement);
-    djfhPreview.innerHTML = ''; // Cleat all fields
+    
     return djfhPreview.insertBefore(previewElement, djfhPreview.childNodes[0]);
 
-    // img.classList.add('preview');
+    
 }
 
 const uploadToServer = (data) => {
@@ -238,3 +263,16 @@ function drop(e) {
     djfhDropFile = files[0];
     djfhInput.value = ''; // Empty file input if a file has been dragged
 }
+
+// function isFileSelected
+
+
+
+//     //document.querySelector("input[type=file]").onchange = function(event) {
+//         //let file = event.target.files[0];
+//         // let file = img.files[0];
+//         let blobURL = URL.createObjectURL(file);
+//         console.log(blobURL);
+//         document.querySelector("video").src = blobURL;
+//         this.uploadToServer(file);
+//     //}
